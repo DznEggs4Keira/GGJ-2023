ff0e6902874cf09716258a96cb001a36bd8a3452
Zainab Ali <47698736+DznEggs4Keira@users.noreply.github.com>
2023-02-03 21:02:59 +0400

    Player misses hit sound fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,6 +25,7 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private float hitRange = 1.2f;
     [SerializeField] private LayerMask enemyLayers;
     [SerializeField] private float recieveHitDelay = 1.5f;
+    
 
     [Header("Simulated Physics Settings")]
     [SerializeField] private Rigidbody2D player_rb;
@@ -41,7 +42,6 @@ public class PlayerController : MonoBehaviour {
     private float delayTimer = 0;
     private bool dead = false;
     private bool isRooted = false;
-    private bool isAttacking = false;
 
     // Awake is called before the first frame
     void Awake()
@@ -75,9 +75,7 @@ public class PlayerController : MonoBehaviour {
 
         // handle player attacking
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1")) {
-            isAttacking = true;
-        } else {
-            isAttacking = false;
+            Attack();
         }
 
         // handle death
@@ -95,10 +93,6 @@ public class PlayerController : MonoBehaviour {
         }
 
         Move();
-
-        if(isAttacking) {
-            Attack();
-        }
     }
 
     void Move() {
@@ -115,84 +109,49 @@ public class PlayerController : MonoBehaviour {
         AudioManager.instance.ReccuringPlay("Tate Footsteps", currentMovement.sqrMagnitude <= 0.01 ? false : true);
     }
 
-    #region Debug Circle 2D
-        public static void DrawCircle(Vector3 position, float radius, int segments, Color color) {
-        // If either radius or number of segments are less or equal to 0, skip drawing
-        if (radius <= 0.0f || segments <= 0) {
-            return;
-        }
 
-        // Single segment of the circle covers (360 / number of segments) degrees
-        float angleStep = (360.0f / segments);
-
-        // Result is multiplied by Mathf.Deg2Rad constant which transforms degrees to radians
-        // which are required by Unity's Mathf class trigonometry methods
-
-        angleStep *= Mathf.Deg2Rad;
-
-        // lineStart and lineEnd variables are declared outside of the following for loop
-        Vector3 lineStart = Vector3.zero;
-        Vector3 lineEnd = Vector3.zero;
-
-        for (int i = 0; i < segments; i++) {
-            // Line start is defined as starting angle of the current segment (i)
-            lineStart.x = Mathf.Cos(angleStep * i);
-            lineStart.y = Mathf.Sin(angleStep * i);
-
-            // Line end is defined by the angle of the next segment (i+1)
-            lineEnd.x = Mathf.Cos(angleStep * (i + 1));
-            lineEnd.y = Mathf.Sin(angleStep * (i + 1));
-
-            // Results are multiplied so they match the desired radius
-            lineStart *= radius;
-            lineEnd *= radius;
-
-            // Results are offset by the desired position/origin 
-            lineStart += position;
-            lineEnd += position;
-
-            // Points are connected using DrawLine method and using the passed color
-            Debug.DrawLine(lineStart, lineEnd, color);
-        }
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(hitPoint.position, hitRange);
     }
-    #endregion
 
     void Attack() {
-
-        //DrawCircle(hitPoint.position, hitRange, 32, Color.red);
 
         // Do attack ...
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(hitPoint.position, hitRange, enemyLayers);
 
         // you collide with nothing, then play miss sound
-        if(hitEnemies != null) {
-            foreach (var enemy in hitEnemies) {
-                // can only attack the trap if rooted
-                if (enemy.gameObject.layer == 7) {
-                    if (!isRooted) {
-                        //Play Missing Sound
-                        AudioManager.instance.Play("Tate Misses", true);
-                        continue;
-                    }
-                }
-
-                var hit = enemy.transform.GetComponent<EnemyController>();
-
-                if (hit != null) {
-                    hit.EnemyHealth -= 10;
-
-                    //Play Attack Sound
-                    AudioManager.instance.Play("Tate Attack", true);
-                }
-
-                //if we destroyed a spawner, track that so that the player can finish game
-                if(enemy.gameObject.layer == 8) {
-                    GameManager.instance.CurrentBossEnemiesKilled++;
-                }
-            }
-        } else {
+        if (hitEnemies == null) {
             //Play Missing Sound
             AudioManager.instance.Play("Tate Misses", true);
+            return;
+        }
+
+        foreach (var enemy in hitEnemies) {
+            // can only attack the trap if rooted
+            if (enemy.gameObject.layer == 7) {
+                if (!isRooted) {
+                    //Play Missing Sound
+                    AudioManager.instance.Play("Tate Misses", true);
+                    continue;
+                }
+            }
+
+            var hit = enemy.transform.GetComponent<EnemyController>();
+
+            // make sure it is indeed the enemy with a script for health on it AND the time between attacvks has passed
+            if (hit != null) {
+                hit.EnemyHealth -= 10;
+
+                //Play Attack Sound
+                AudioManager.instance.Play("Tate Attack", true);
+
+            }
+
+            //if we destroyed a spawner, track that so that the player can finish game
+            if(enemy.gameObject.layer == 8) {
+                GameManager.instance.CurrentBossEnemiesKilled++;
+            }
         }
     }
 
