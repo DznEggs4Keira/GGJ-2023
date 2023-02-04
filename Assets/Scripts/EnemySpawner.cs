1efe7fd85bec1190c5df67a762322151266fb02e
Ian Rue <74080218+IanRue@users.noreply.github.com>
2023-02-04 17:24:32 -0300

    Test

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,11 +3,17 @@ using UnityEngine.Pool;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [Header("Player Information")]
+    [SerializeField] private Transform player;
+
     [Header("Enemy Prefab")]
     [SerializeField] private EnemyController enemyPrefab;
     [SerializeField] private int spawnAmmount = 10;
     [SerializeField] private int currentSpawn;
     [SerializeField] private int maxSpawnAmmount = 50;
+
+    [Header("Spawner Range")]
+    public float maxDistance;
 
     [SerializeField] private ObjectPool<EnemyController> enemyPool;
     // Start is called before the first frame update
@@ -20,13 +26,22 @@ public class EnemySpawner : MonoBehaviour
         }, enemy => { Destroy(enemy.gameObject);
         },true, 10, maxSpawnAmmount);
 
+        // get player position
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
         InvokeRepeating(nameof(Spawn), 0.2f, 5f);
     }
 
     void Spawn() {
 
+        // cap the max amount of mushrooms on screen at a time
         if(currentSpawn >= maxSpawnAmmount) {
             currentSpawn = 0;
+            return;
+        }
+
+        // check if the player is nearby
+        if((transform.position - player.position).magnitude >= maxDistance) {
             return;
         }
 
@@ -43,5 +58,10 @@ public class EnemySpawner : MonoBehaviour
     void Kill(EnemyController enemy) {
         AudioManager.instance.Play("Shroom Dies", true);
         enemyPool.Release(enemy);
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, maxDistance / 2);
     }
 }
